e49810167b54cc04b6e55adb83d1bf56d030529d
Maxeem <jmax.posh@gmail.com>
2023-10-09 16:39:36 -0300

    ensure that mocked comment added on click FAB

--- test/challenge3/view/comments_view_test.dart
+++ test/challenge3/view/comments_view_test.dart
@@ -1,17 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:spectora_interview_code_flutter/challenge3/models/comment.dart';
+import 'package:spectora_interview_code_flutter/challenge3/providers/viewmodel_providers.dart';
+import 'package:spectora_interview_code_flutter/challenge3/repositories/comments_repository.dart';
 import 'package:spectora_interview_code_flutter/challenge3/view/comments_view.dart';
 
 import '../../test_helpers.dart';
 
 void main() {
   group('CommentsView', () {
-    testWidgets('When tap on FAB it adds a new comment to the ListView',
+    late ProviderContainer providerContainer;
+
+    setUpAll(() {
+      providerContainer = ProviderContainer();
+    });
+
+    tearDownAll(() {
+      providerContainer.dispose();
+    });
+
+    testWidgets('When tap on FAB it adds new comment to the ListView',
         (WidgetTester tester) async {
+      const mockComment = Comment(
+          id: '000',
+          value: 0.12345,
+          name: 'Mock Comment 1',
+          description: 'Mock Desc. 1');
+
+      final mockRepository = MockCommentsRepository();
+      when(() => mockRepository.getRandom())
+          .thenAnswer((_) => Future.value(mockComment));
+
       await tester.pumpWidget(
-        const ProviderScope(
-          child: MaterialApp(
+        ProviderScope(
+          overrides: [
+            ViewModelProviders.commentsRepositoryProvider
+                .overrideWithValue(mockRepository)
+          ],
+          child: const MaterialApp(
             home: CommentsView(),
           ),
         ),
@@ -26,33 +54,25 @@ void main() {
           .widget<ListView>(listViewFind)
           .childrenDelegate as SliverChildBuilderDelegate;
 
+      // Ensures mock that ListView is empty without mock comment
       expect(childrenDelegate.childCount, 0);
+      expect(find.text(mockComment.value.toString()), findsNothing);
+      expect(find.text(mockComment.name), findsNothing);
+      expect(find.text(mockComment.description), findsNothing);
 
+      // Tap on Floating Action Button to add mock comment to ListView
       await tester.tap(find.byType(FloatingActionButton));
       await tester.pumpAndSettle();
 
+      // Ensures mock comment added to the ListView
       childrenDelegate = tester.widget<ListView>(listViewFind).childrenDelegate
           as SliverChildBuilderDelegate;
-
       expect(childrenDelegate.childCount, 1);
-
-      // final textFinder = find.byKey(HeavyOperationView.numberTextKey);
-      // expect(textFinder, findsOneWidget);
-
-      // // Wait until the first number text populated
-      // await untilTrue(() {
-      //   final text = tester.widget<Text>(textFinder).data;
-      //   return text != '';
-      // });
-
-      // // Ensure that text is not empty
-      // final text = tester.widget<Text>(textFinder).data;
-      // expect(text, isNotNull);
-
-      // // Ensure that text is number and positive
-      // final number = int.tryParse(text!);
-      // expect(number, isA<int>());
-      // expect(number, isPositive);
+      expect(find.text(mockComment.value.toString()), findsOneWidget);
+      expect(find.text(mockComment.name), findsOneWidget);
+      expect(find.text(mockComment.description), findsOneWidget);
     });
   });
 }
+
+class MockCommentsRepository extends Mock implements CommentsRepository {}
