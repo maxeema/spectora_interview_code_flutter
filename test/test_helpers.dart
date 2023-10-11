518ef696b0ed7e438b79a80ee1ee69baece364e8
Maxeem <jmax.posh@gmail.com>
2023-10-11 20:07:51 -0300

    extends WidgetTester with waitFor(), waitForNo(), untilTrue()

--- test/test_helpers.dart
+++ test/test_helpers.dart
@@ -1,18 +1,43 @@
 import 'package:flutter_test/flutter_test.dart';
 
-/// Waits until the given predicate returns true.
-/// Note! Avoid using this call directly in widget tests, e.g. in testWidgets()
-/// where await Future.delayed() freezes the test execution.
-Future<void> untilTrue(bool predicate(), {Duration? interval}) async {
-  while (!predicate()) {
-    await Future.delayed(interval ?? const Duration(milliseconds: 50));
+extension WidgetTesterX on WidgetTester {
+  /// Wait for widget appearance in the widget tree.
+  /// Use it like that:
+  ///  await tester.waitFor(find.byKey(key));
+  Future waitFor(Finder finder) async {
+    while (finder.evaluate().isEmpty) {
+      await pump(const Duration(milliseconds: 20));
+    }
+  }
+
+  /// Wait for widget removal from the widget tree.
+  /// Use it like that:
+  ///  await tester.waitFor(find.byKey(key));
+  Future waitForNo(Finder finder) async {
+    while (finder.evaluate().isNotEmpty) {
+      await pump(const Duration(milliseconds: 20));
+    }
+  }
+
+  /// Wait until the predicate() returned true first time.
+  /// Use it like that:
+  ///  await tester.untilTrue(() => ...);
+  Future untilTrue(bool predicate()) async {
+    while (!predicate()) {
+      await pump(const Duration(milliseconds: 20));
+    }
   }
 }
 
-/// Waits in widget testing until the given predicate returns true.
-Future<void> until(WidgetTester tester, bool predicate(),
-    {Duration? interval}) async {
+/// Waits until the given predicate returns true.
+///
+/// Note! Avoid using this call directly in widget tests, e.g. in testWidgets()
+/// where the "await Future.delayed(..)" call freezes the test execution
+/// because Flutter widget tests are ran in a FakeAsync (mock clock) by default.
+/// Otherwise, use it like that:
+///  await tester.runAsync(() => untilTrue(() => ...));
+Future<void> untilTrue(bool predicate(), {Duration? interval}) async {
   while (!predicate()) {
-    await tester.pump(interval ?? const Duration(milliseconds: 50));
+    await Future.delayed(interval ?? const Duration(milliseconds: 50));
   }
 }
