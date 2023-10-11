39ae30310ebf7018feba2967c8d9dc199c11d2a6
Maxeem <jmax.posh@gmail.com>
2023-10-11 03:37:18 -0300

    add until() helper method in widgets tests

--- test/test_helpers.dart
+++ test/test_helpers.dart
@@ -1,6 +1,18 @@
+import 'package:flutter_test/flutter_test.dart';
+
 /// Waits until the given predicate returns true.
-Future<void> untilTrue(bool predicate(), {Duration? checkInterval}) async {
+/// Note! Avoid using this call directly in widget tests, e.g. in testWidgets()
+/// where await Future.delayed() freezes the test execution.
+Future<void> untilTrue(bool predicate(), {Duration? interval}) async {
   while (!predicate()) {
-    await Future.delayed(checkInterval ?? const Duration(milliseconds: 50));
+    await Future.delayed(interval ?? const Duration(milliseconds: 50));
+  }
+}
+
+/// Waits in widget testing until the given predicate returns true.
+Future<void> until(WidgetTester tester, bool predicate(),
+    {Duration? interval}) async {
+  while (!predicate()) {
+    await tester.pump(interval ?? const Duration(milliseconds: 50));
   }
 }
