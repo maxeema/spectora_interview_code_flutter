6023f818853136465295545ed85587f046477d3a
Maxeem <jmax.posh@gmail.com>
2023-10-12 10:15:39 -0300

    use universal_io to access Platform.xxx everywhere including web

--- lib/runtime_info.dart
+++ lib/runtime_info.dart
@@ -1,16 +1,15 @@
-import 'dart:io';
-
 import 'package:flutter/foundation.dart';
 
+import 'package:universal_io/io.dart';
+
 abstract class RuntimeInfo {
-  /// Indicates that the app is ran by the Flutter test framework
+  /// Indicates that code is ran by the Flutter test framework
   static final bool isFlutterTest =
-      !kIsWeb && Platform.environment.containsKey('FLUTTER_TEST');
+      Platform.environment.containsKey('FLUTTER_TEST');
 
   /// Indicates if we can use Isolates or they don't make sense.
   /// In Fluter web there is only a way to use Web Workers.
   /// In Flutter widget testing executing code "await compute()"
   /// will lead to nothing (await call won't just finish).
-  static final bool canUseIsolates =
-      !(kIsWeb || Platform.environment.containsKey('FLUTTER_TEST'));
+  static final bool canUseIsolates = !(kIsWeb || isFlutterTest);
 }
